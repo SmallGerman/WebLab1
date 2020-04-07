65a35fc580e3cc5e9b0cbf071f291b40fd560166
Apcikk <arsenyakym@gmail.com>
2020-04-07 17:17:50 +0300

    did task 2

--- Task 2.js
+++ Task 2.js
@@ -1,1 +1,17 @@
-
+exports.CharUp = (text) => {
+    function firstCharUp(sentence) {
+        let CountLetter = '',
+            SpacePos = 0,
+            UpLetter = '';
+        while (true) {
+            SpacePos = sentence.indexOf(' ', SpacePos + 1);
+            CountLetter = sentence[SpacePos+1].toUpperCase();
+            if (SpacePos === -1){
+                break;
+            }
+            UpLetter = `${UpLetter}${CountLetter} `;
+        }
+        return `${sentence[0].toUpperCase()} ` + UpLetter;
+    }
+    console.log(firstCharUp('the war in thailand characterises hell'));
+};
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+const {CharUp} = require('./task 2');
+
 // Task 1
 let bio = ['im', 'smallgerman', 'or', 'apcikk', 'but', 'call', 'me', 'boss', 'of', 'the', 'gym'];
 console.log(bio.join(' '));
 
-
+// Task 2
+CharUp();
