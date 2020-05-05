b9ae61a6fe7a77f5507007bc202e3df4befe6932
Apcikk <arsenyakym@gmail.com>
2020-05-05 13:21:33 +0300

    Task 7 done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {CharUp} = require('./Task 2');
 const {Fib} = require('./Task 3');
 const {Unique} = require('./Task 4');
+const {subtraction} = require('./Task 5');
 const {Split} = require('./Task 6');
 
 // Task 1
@@ -20,6 +21,13 @@ console.log(Fib(5));
 // Task 4
 let arr = ['lol', 'kek', 'lmao', 'kek', 'kek', 'kekw', 'lol'];
 console.log(Unique(arr));
+
+// Task 5
+let first = new Date(2020, 11, 12); //month from 0
+let second = new Date(2019, 11, 5);
+
+
+subtraction(first, second);
 
 // Task 6
 let array = [5, 1, 9, 2, 6, 2, 7];
